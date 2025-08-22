5f2dfc0f256f2e3f7d421dc6698e4d0c50ff784d
sryang <sarang628@gmail.com>
2025-08-22 18:58:33 +0800

    update

--- CustomVideoPlayerType.kt
+++ CustomVideoPlayerType.kt
@@ -1,6 +1,5 @@
 package com.sarang.torang.di.basefeed_di
 
-import com.sarang.torang.VideoPlayerScreen
 import com.sarang.torang.compose.feed.internal.components.VideoPlayerType
 
 val CustomVideoPlayerType: VideoPlayerType = {
