cc3eae2fcaa98dd4f41dab61e9e07607464472b8
sryang <sarang628@gmail.com>
2025-09-25 08:42:06 +0800

    block click top and bottom in image

--- CustomFeedImageLoader.kt
+++ CustomFeedImageLoader.kt
@@ -1,6 +1,18 @@
 package com.sarang.torang.di.basefeed_di
 
 import com.sarang.torang.compose.feed.internal.components.FeedImageLoader
+import com.sarang.torang.di.image.ImageLoadData
 import com.sarang.torang.di.image.provideZoomableTorangAsyncImage
 
-val CustomFeedImageLoader: FeedImageLoader = provideZoomableTorangAsyncImage()
+val CustomFeedImageLoader: FeedImageLoader = { data ->
+    provideZoomableTorangAsyncImage().invoke(
+        ImageLoadData(
+            modifier = data.modifier,
+            url = data.url,
+            progressSize = data.progressSize,
+            errorIconSize = data.errorIconSize,
+            contentScale = data.contentScale,
+            height = data.height
+        )
+    )
+}
