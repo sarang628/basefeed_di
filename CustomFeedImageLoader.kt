ff69f84f92dacbd97ca1e41e65f146b9b21fb5be
sryang <sarang628@gmail.com>
2025-10-22 10:35:36 +0900

    update pinch zoom image loader

--- CustomFeedImageLoader.kt
+++ CustomFeedImageLoader.kt
@@ -1,11 +1,33 @@
 package com.sarang.torang.di.basefeed_di
 
+import com.example.pinchzoom.library.pinchzoom.rememberPinchZoomState
+import com.example.pinchzoom.submodule.pinchzoom.PunchZoomImageData
 import com.sarang.torang.compose.feed.internal.components.FeedImageLoader
 import com.sarang.torang.di.image.ImageLoadData
 import com.sarang.torang.di.image.provideZoomableTorangAsyncImage
+import com.sarang.torang.di.pinchzoom.PinchZoomState
+import com.sarang.torang.di.pinchzoom.pinchZoomImageLoader
 
-val CustomFeedImageLoader: FeedImageLoader = { data ->
-    provideZoomableTorangAsyncImage().invoke(
+fun CustomFeedImageLoader(
+    zoomState: PinchZoomState?,
+    onZoomState : (PinchZoomState?)->Unit  ={},
+    showLog     : Boolean = false,
+): FeedImageLoader = { data ->
+
+    pinchZoomImageLoader(
+        zoomState = zoomState,
+        showLog = showLog,
+        onZoomState = onZoomState
+    ).invoke(
+        PunchZoomImageData(
+            modifier = data.modifier,
+            model = data.url ?: "",
+            contentScale = data.contentScale,
+            contentDescription = null
+        )
+    )
+
+    /*provideZoomableTorangAsyncImage().invoke(
         ImageLoadData(
             modifier = data.modifier,
             url = data.url,
@@ -14,5 +36,5 @@ val CustomFeedImageLoader: FeedImageLoader = { data ->
             contentScale = data.contentScale,
             height = data.height
         )
-    )
+    )*/
 }
