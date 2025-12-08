2a14653da351d85ee97e1119e5152021f1fab0e5
sryang <sarang628@gmail.com>
2025-12-08 20:48:40 +0900

    add my favorite

--- DataConverter.kt
+++ DataConverter.kt
@@ -7,18 +7,18 @@ import com.sarang.torang.data.basefeed.FeedItemUiState
 fun ReviewAndImageEntity.toReview(): FeedItemUiState {
     return FeedItemUiState(
         reviewImages        = this.images.map { BuildConfig.REVIEW_IMAGE_SERVER_URL + it.pictureUrl },
-        userName            = this.review.userName,
+        userName            = this.review.userName ?: "",
         profilePictureUrl   = BuildConfig.PROFILE_IMAGE_SERVER_URL + this.review.profilePicUrl,
         restaurantName      = this.review.restaurantName ?: "",
-        rating              = this.review.rating,
-        likeAmount          = this.review.likeAmount,
-        commentAmount       = this.review.commentAmount,
+        rating              = this.review.rating ?: 0f,
+        likeAmount          = this.review.likeAmount ?: 0,
+        commentAmount       = this.review.commentAmount ?: 0,
         comments            = listOf(),
         isLike              = this.like != null,
         isFavorite          = this.favorite != null,
-        contents            = this.review.contents,
-        height              = if(images.isEmpty()) 300 else this.images[0].height,
-        width               = if(images.isEmpty()) 300 else this.images[0].width,
-        createDate          = this.review.createDate
+        contents            = this.review.contents ?: "",
+        height              = if(images.isEmpty()) 300 else this.images[0].height ?: 0,
+        width               = if(images.isEmpty()) 300 else this.images[0].width ?: 0,
+        createDate          = this.review.createDate ?: ""
     )
 }
