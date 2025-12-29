a9894647d457306b6f652f7de309cae72dae023b
sryang <sarang628@gmail.com>
2025-12-29 23:35:51 +0900

    arrange code

--- CustomExpandableTextType.kt
+++ CustomExpandableTextType.kt
@@ -3,6 +3,9 @@ package com.sarang.torang.di.basefeed_di
 import com.sarang.torang.compose.feed.internal.components.type.ExpandableTextType
 import com.sryang.library.ExpandableText
 
-val CustomExpandableTextType: ExpandableTextType = { modifier, nickName, text, onClickNickName ->
-    ExpandableText(modifier, nickName, text, onClickNickName)
+val CustomExpandableTextType: ExpandableTextType = {
+    ExpandableText(modifier = it.modifier,
+                   nickName = it.nickName,
+                   text = it.contents,
+                   onClickNickName = it.onNickName)
 }
