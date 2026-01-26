29b31e8587b12e77bd798927fea22b5359aa01af
sryang <sarang628@gmail.com>
2026-01-26 15:18:09 +0900

    update video player

--- CustomVideoPlayerType.kt
+++ CustomVideoPlayerType.kt
@@ -1,7 +1,10 @@
 package com.sarang.torang.di.basefeed_di
 
+import com.sarang.torang.VideoPlayer
 import com.sarang.torang.compose.feed.internal.components.type.VideoPlayerType
 
 val CustomVideoPlayerType: VideoPlayerType = {
-    VideoPlayerScreen(videoUrl = it, isPlaying = true, onClick = {}, onPlay = {})
+    //VideoPlayerScreen(videoUrl = it, isPlaying = true, onClick = {}, onPlay = {})
+    VideoPlayer(videoUrl        = it.url,
+                playWhenReady   = it.isPlaying)
 }
