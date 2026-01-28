822cbd68fd51bb1fdde4a7dbe0fa02fcfb304caf
sryang <sarang628@gmail.com>
2026-01-28 19:21:42 +0900

    update modules

--- CustomVideoPlayerType.kt
+++ CustomVideoPlayerType.kt
@@ -1,16 +1,10 @@
 package com.sarang.torang.di.basefeed_di
 
-import android.util.Log
-import androidx.compose.runtime.LaunchedEffect
 import com.sarang.torang.VideoPlayer
 import com.sarang.torang.compose.feed.internal.components.type.VideoPlayerType
 
 fun CustomVideoPlayerType(onClick : () -> Unit = {}): VideoPlayerType = {
     //VideoPlayerScreen(videoUrl = it, isPlaying = true, onClick = {}, onPlay = {})
-
-    LaunchedEffect(it.isPlaying) {
-        Log.d("__CustomVideoPlayerType", "isPlaying : ${it.isPlaying}")
-    }
 
     VideoPlayer(videoUrl        = it.url,
                 playWhenReady   = it.isPlaying,
