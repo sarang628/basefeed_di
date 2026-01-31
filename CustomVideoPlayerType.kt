8a8a3e459a8ad926bd1a6acbb5013e20e32b366d
sryang <sarang628@gmail.com>
2026-01-31 12:24:06 +0900

    when scrolling feed video keep playing which is already loaded

--- CustomVideoPlayerType.kt
+++ CustomVideoPlayerType.kt
@@ -3,10 +3,10 @@ package com.sarang.torang.di.basefeed_di
 import com.sarang.torang.VideoPlayer
 import com.sarang.torang.compose.feed.internal.components.type.VideoPlayerType
 
-fun CustomVideoPlayerType(onClick : () -> Unit = {}): VideoPlayerType = {
-    //VideoPlayerScreen(videoUrl = it, isPlaying = true, onClick = {}, onPlay = {})
-
+fun CustomVideoPlayerType(onClick   : () -> Unit = {},
+                          onPlayed  : () -> Unit = {}): VideoPlayerType = {
     VideoPlayer(videoUrl        = it.url,
                 playWhenReady   = it.isPlaying,
-                onClick         = onClick)
+                onClick         = onClick,
+                onPlayed        = onPlayed)
 }
