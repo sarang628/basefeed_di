8648d2460f094180cc0d2cc2acba715462c67943
sryang <sarang628@gmail.com>
2026-02-01 13:19:00 +0900

    add thumbnail

--- CustomVideoPlayerType.kt
+++ CustomVideoPlayerType.kt
@@ -1,12 +1,36 @@
 package com.sarang.torang.di.basefeed_di
 
+import TorangAsyncImage
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import com.sarang.torang.LocalThumbImageLoader
+import com.sarang.torang.ShortItem
+import com.sarang.torang.ShortVideo
 import com.sarang.torang.VideoPlayer
 import com.sarang.torang.compose.feed.internal.components.type.VideoPlayerType
 
 fun CustomVideoPlayerType(onClick   : () -> Unit = {},
                           onPlayed  : () -> Unit = {}): VideoPlayerType = {
-    VideoPlayer(videoUrl        = it.url,
-                playWhenReady   = it.isPlaying,
-                onClick         = onClick,
-                onPlayed        = onPlayed)
+                              CompositionLocalProvider(
+                                  LocalThumbImageLoader provides {
+                                      TorangAsyncImage(
+                                          modifier = it.modifier.fillMaxSize(),
+                                          model = it.url
+                                      )
+                                  }
+                              ) {
+                                  ShortItem(
+                                      short = ShortVideo(id            = it.url,
+                                                         videoUrl      = it.url,
+                                                         thumbNailUrl  = it.url.substringBeforeLast(".") + ".jpg"),
+                                      isActive = it.isPlaying,
+                                      onClick = onClick,
+                                      onPlayed = onPlayed
+                                  )
+                                  VideoPlayer(videoUrl        = it.url,
+                                      playWhenReady   = it.isPlaying,
+                                      onClick         = onClick,
+                                      onPlayed        = onPlayed)
+                              }
 }
