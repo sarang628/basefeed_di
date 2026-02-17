04b8d87bafb33a37a91ab6a8ebd010cdcd19e19d
sryang <sarang628@gmail.com>
2026-02-17 09:05:59 +0900

    feed top refactoring

--- DataConverter.kt
+++ DataConverter.kt
@@ -1,6 +1,7 @@
 package com.sarang.torang.di.basefeed_di
 
 import com.sarang.torang.BuildConfig
+import com.sarang.torang.compose.feed.internal.components.FeedTopUiState
 import com.sarang.torang.core.database.model.feed.ReviewAndImageEntity
 import com.sarang.torang.data.ReviewAndImage
 import com.sarang.torang.data.basefeed.FeedItemUiState
@@ -8,10 +9,10 @@ import com.sarang.torang.data.basefeed.FeedItemUiState
 fun ReviewAndImage.toReview(): FeedItemUiState {
     return FeedItemUiState(
         reviewImages        = this.images.map { BuildConfig.REVIEW_IMAGE_SERVER_URL + it.pictureUrl },
-        userName            = this.review.userName ?: "",
-        profilePictureUrl   = BuildConfig.PROFILE_IMAGE_SERVER_URL + this.review.profilePicUrl,
-        restaurantName      = this.review.restaurantName ?: "",
-        rating              = this.review.rating ?: 0f,
+        feedTopUiState = FeedTopUiState(userName            = this.review.userName ?: "",
+                                        profilePictureUrl   = BuildConfig.PROFILE_IMAGE_SERVER_URL + this.review.profilePicUrl,
+                                        restaurantName      = this.review.restaurantName ?: "",
+                                        rating              = this.review.rating ?: 0f,),
         likeAmount          = this.review.likeAmount ?: 0,
         commentAmount       = this.review.commentAmount ?: 0,
         comments            = listOf(),
